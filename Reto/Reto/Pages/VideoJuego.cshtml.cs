7ed2f3111d8e16ef72f3c8c2b65fa47eaa356e08
Santiago Paredes Suarez <santiparedes2004@gmail.com>
2025-04-27 16:22:34 -0600

    Mejore diseño general
    disculpenme ahi por los conflictos que puedan existir

---
+++ Reto/Reto/Pages/VideoJuego.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Reto.Model;
+using Microsoft.AspNetCore.Http;
+
+namespace Reto.Pages
+{
+    public class VideoJuegoModel : PageModel
+    {
+        
+    }
+}
